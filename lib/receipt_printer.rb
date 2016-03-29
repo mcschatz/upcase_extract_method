410ff597e15da92494196b3ac851376ce5db9ee7
Mimi Schatz <mcschatz@gmail.com>
2016-03-28 21:18:05 -0400

    extracted subtotal into own method

--- lib/receipt_printer.rb
+++ lib/receipt_printer.rb
@@ -13,27 +13,31 @@ class ReceiptPrinter
   end
 
   def print
-    subtotal = items.reduce(0) do |sum, item|
-      item_cost = COST[item]
-      output.puts "#{item}: #{sprintf('$%.2f', item_cost)}"
-      sum + item_cost.to_i
-    end
-    print_receipt_total(subtotal)
-  end
-
-  def print_receipt_total(subtotal)
-    output.puts divider
-    output.puts "subtotal: #{sprintf('$%.2f', subtotal)}"
-    output.puts "tax: #{sprintf('$%.2f', subtotal * TAX)}"
-    output.puts divider
-    output.puts "total: #{sprintf('$%.2f', subtotal + (subtotal * TAX))}"
+    receipt_output(calculate_subtotal)
   end
 
   private
 
+    def receipt_output(subtotal)
+      output.puts divider
+      output.puts "subtotal: #{sprintf('$%.2f', subtotal)}"
+      output.puts "tax: #{sprintf('$%.2f', subtotal * TAX)}"
+      output.puts divider
+      output.puts "total: #{sprintf('$%.2f', subtotal + (subtotal * TAX))}"
+    end
+
+    def calculate_subtotal
+      items.reduce(0) do |sum, item|
+        item_cost = COST[item]
+        output.puts "#{item}: #{sprintf('$%.2f', item_cost)}"
+        sum + item_cost.to_i
+      end
+    end
+
+    def divider
+      '-' * 13
+    end
+
   attr_reader :output, :items
 
-  def divider
-    '-' * 13
-  end
 end
